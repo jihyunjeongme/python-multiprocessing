05efe378b579c81362bdfc27c961d6e5eac3a08f
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-04-03 23:15:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,46 @@
-# 피보나치 수를 구해주는 프로그램
-# # 싱글 스레드
-# import time
-
-# start_time = time.time()
-
-
-# def fibo(n):  # 회귀적 피보나치 함수
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     else:
-#         return fibo(n - 1) + fibo(n - 2)
-
-
-# num_list = [31, 32, 33, 34]
-# result_list = []
-# for num in num_list:
-#     result_list.append(fibo(num))
-
-# print(result_list)
-# print("----%s seconds----" % (time.time() - start_time))
-
-## 멀티프로세싱
-from multiprocessing import Pool
+# 쿠팡 화장품 멀티프로세싱으로 가져오기
+import requests
+from bs4 import BeautifulSoup
 import time
 
-start_time = time.time()
+from multiprocessing import Pool
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+}
+
+# 가격표에 게시글 링크를 가져옵니다.
+def get_links():
+    data = []
+
+    for idx in range(1, 17):
+        url = "http://www.coupang.com/np/categories/176860?page=" + str(idx)
+        data.append(url)
+    return data
 
 
-def fibo(n):  # 회귀적 피보나치 함수
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibo(n - 1) + fibo(n - 2)
+# 상품과 가격을 가져옵니다.
+def get_content(link):
+    url = link
+    result = requests.get(url, headers=headers)
+
+    soup_obj = BeautifulSoup(result.content, "html.parser")
+
+    div = soup_obj.findAll("div", {"class": "name"})
+    lis = soup_obj.find("ul", {"id": "productList"}).findAll("li")
+
+    for li in lis:
+        product = li.find("div", {"class": "name"})
+        price = li.find("em", {"class": "sale"}).find(
+            "strong", {"class": "price-value"}
+        )
+        print("화장품명: " + product.text.strip() + " / " + "상품가격: " + price.text.strip())
 
 
-def print_fibo(n):  # 피보나치 결과를 확인 합니다.
-    print(fibo(n))
-
-
-num_list = [31, 32, 33, 34]
-
-pool = Pool(processes=4)  # 4개의 프로세스를 사용합니다.
-pool.map(print_fibo, num_list)  # pool에 일을 던져 줍니다.
-
-print("----%s seconds----" % (time.time() - start_time))
-
-## 쿠팡 쇼핑몰 크롤링 로직 추가
-# 쿠팡 화장 품 크롤링
+if __name__ == "__main__":
+    start_time = time.time()
+    # 32개 프로세스 사용 / 테스트 결과 ----3.2799859046936035 seconds-----
+    pool = Pool(processes=32)
+    pool.map(get_content, get_links())
+    print("----%s seconds-----" % (time.time() - start_time))
 
